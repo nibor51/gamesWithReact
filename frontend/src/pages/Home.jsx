44b7572731a25c349b43e97cdfede1f02d7e3951
Nibor51 <jonval.robin@gmail.com>
2022-04-14 16:23:46 +0200

    display the component case

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,22 @@
-import Counter from "@components/Counter";
 import logo from "@assets/logo.svg";
+import Case from "@components/ticTacToe/Case";
 
 export default function Home() {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
+      <h1>Tic-Tac-Toe</h1>
+      <div className="flex">
+        <Case />
+        <Case />
+        <Case />
+        <Case />
+        <Case />
+        <Case />
+        <Case />
+        <Case />
+        <Case />
+      </div>
     </header>
   );
 }
